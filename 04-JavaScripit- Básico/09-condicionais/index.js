ddf093e68f77beaf48c3ac5a29032796d82e8780
Vitor Lopes de Matos <vitorlopesdematos523@gmail.com>
2022-08-25 09:47:39 -0400

    adicionei conteúdo do módulo de javascrip básico

---
+++ 04-JavaScripit- Básico/09-condicionais/index.js
@@ -0,0 +1,12 @@
+let nomeFilme = 'Batman vs Surpeman'
+
+if(nomeFilme === 'Vingadores') {
+  console.log('É o filme dos Vingadores')
+}else if(nomeFilme === 'Batman vs Surpeman') {
+  console.log('É o filme do Batman vs Surpeman')
+} else {
+  console.log('É outro filme')
+}
+
+//Operador Tenário
+nomeFilme === 'vingadores'? console.log('É o filme dos Vingadores') : console.log('É outro filme')
