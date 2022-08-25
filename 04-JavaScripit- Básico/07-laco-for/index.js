20e426d861aacea2f2dc28d90e848174f758a577
Vitor Lopes de Matos <vitorlopesdematos523@gmail.com>
2022-08-24 21:01:02 -0400

    adicionei conteúdo do módulo de javascript básico

---
+++ 04-JavaScripit- Básico/07-laco-for/index.js
@@ -0,0 +1,9 @@
+/*
+      for(inicializador; condição; incremento){
+        declaração
+      }
+*/
+
+for(let i = 0; i < 5; i++) {
+  console.log(i)
+}
---
+++ 04-JavaScripit- Básico/08-laco-while/index.js
@@ -0,0 +1,26 @@
+/*
+    while(condição) {
+      bloco de execução
+    }
+*/
+
+let a = 10
+let b = 15
+
+while(a < b) {
+  console.log('incrementando a variável a ' + a)
+  a++;
+}
+
+/*
+    do {
+      bloco de execução
+    }while(condição)
+*/
+
+var i = 1
+
+do {
+  console.log('Entrou ' + i)
+  i++
+}while(i <= 5)
