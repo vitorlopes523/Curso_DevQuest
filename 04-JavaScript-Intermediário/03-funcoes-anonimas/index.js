ecf40d17d527d2945b9055f8837f0c1ddd38a5f9
Vitor Lopes de Matos <vitorlopesdematos523@gmail.com>
2022-08-30 09:50:38 -0400

    adicionei conteúdo de javascript intermediário

---
+++ 04-JavaScript-Intermediário/03-funcoes-anonimas/index.js
@@ -0,0 +1,5 @@
+let resultadoDaSoma = function(numero1, numero2) {
+  return numero1 + numero2
+}
+
+console.log(resultadoDaSoma(12, 5))
---
+++ 04-JavaScript-Intermediário/04-arrow-functions/index.js
@@ -0,0 +1,15 @@
+// let resultadoDaSoma = (numero1, numero2) => {
+//   return numero1 + numero2
+// }
+
+// console.log(resultadoDaSoma(12, 5))
+let resultadoDaSoma = (numero1, numero2) => numero1 + numero2
+
+console.log(resultadoDaSoma(12, 5))
+
+
+const incentivarQuester = nomeQuester => {
+  console.log('Parabéns ' + nomeQuester + ', você chegou ao módulo de Javascript intermediário')
+}
+
+incentivarQuester('Vitor')
