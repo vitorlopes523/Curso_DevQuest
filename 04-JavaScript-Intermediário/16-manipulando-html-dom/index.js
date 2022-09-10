239c263776910a426a1ec07c90d756d1b212dcb1
Vitor Lopes de Matos <vitorlopesdematos523@gmail.com>
2022-09-10 12:21:52 -0400

    adicionei conteúdo do módulo de javascript intermediário

---
+++ 04-JavaScript-Intermediário/16-manipulando-html-dom/index.js
@@ -0,0 +1,9 @@
+function adicionarComentario() {
+  let inputComentario = document.getElementsByName('novo-comentario')
+
+  let textoDigitado = inputComentario[0].value
+
+  let novosComentarios = document.getElementById('novos-comentarios')
+
+  novosComentarios.innerHTML += `<p>Novo comentário: ${textoDigitado} </p>`
+}
