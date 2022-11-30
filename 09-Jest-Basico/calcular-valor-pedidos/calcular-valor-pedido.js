49a009315e141a1a0e9a283891a12a773e2d8c69
Vitor Lopes de Matos <vitorlopesdematos523@gmail.com>
2022-11-29 21:17:27 -0400

    adicionei edições em pastas

---
+++ 09-Jest-Basico/calcular-valor-pedidos/calcular-valor-pedido.js
@@ -0,0 +1,23 @@
+const meuPedido = {
+  itens: [
+    { nome: 'Poção de vida', valor: 100},
+    { nome: 'Espada de prata', valor: 1000},
+    { nome: 'Entrega', valor: 40, entrega: true }
+  ]
+}
+
+const calcularValorPedido = pedido => {
+  const valorProdutos = pedido.itens
+  .filter(item => !item.entrega)
+  .reduce((totalPedidos, pedidoAtual) => totalPedidos + pedidoAtual.valor, 0)
+
+  const entrega = pedido.itens.filter(item => item.entrega)
+
+  if(valorProdutos > 500) {
+    return valorProdutos
+  } else {
+    return valorProdutos + entrega[0].valor
+  }
+}
+
+console.log(calcularValorPedido(meuPedido))
